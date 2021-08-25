515ac2acdce42a5e4fb8cf755968d884269938b6
CJ Barnes <cristopher.barnes@gmail.com>
2021-08-25 19:58:39 -0400

    addToCollection complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+// Create a variable collection that starts as an empty array.
+let collection = [];
+
+// Add a function named addToCollection. This function should:
+const addToCollection = (title, artist, yearPublished) => {
+    // Take in the album's title, artist, yearPublished as input parameters
+    // Create a new object having the above properties
+    let newObject = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    // Add the new object to the end of the collection array
+    collection.push(newObject);
+    // Return the newly created object
+    return newObject;
+}
+
